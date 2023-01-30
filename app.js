533f0326e8d9f83f3aed1f98d46a8aaebfceaa57
antonz89 <anton.zakoverya@codeimmersives.com>
2023-01-30 10:08:07 -0500

    updating

--- app.js
+++ app.js
@@ -21,10 +21,63 @@ const express = require('express')
 			lastModified: new Date()
 		}];
 
-		app.get('/', (req, res) => {
-			res.send('Hello World!')
-		})
+		
 
 		app.listen(port, () => {
 			console.log(`Example app listening on port ${port}`)
 		})
+
+//------------------------------------------------------------
+
+		app.get('/all-movies', (req, res) => {
+			res.json({
+				sucess:true,
+				favoriteMovieList
+			})
+            
+		})
+//------------------------------------------------------------
+		app.get('/single-movie/:titleToFind',(req, res)=>{
+			let foundMovie = favoriteMovieList.find((movie)=>{
+				return movie.title === req.params.titleToFind
+			})
+
+			res.json({
+				success:true,
+				foundMovie
+			})
+		})
+
+//------------------------------------------------------------
+		app.post('/new-movie',(req, res)=>{
+			const newMovie = {}
+			newMovie.title = req.body.title,
+			newMovie.starRating = req.body.starRating,
+			newMovie.isRecomended = req.body.isRecomended,
+			newMovie.reatedAt =  new Date(),
+			newMovie.lastModified =  new Date()
+
+			favoriteMovieList.push(newMovie)
+
+			res.json({
+				success: true
+			})
+		})
+
+//------------------------------------------------------------
+		app.delete("delete-movie/:titleToDelte",(req,res)=>{
+			const titleToDelete = req.params.title
+
+			const deleteMovieIndex = favoriteMovieList.findIndex((movie)=>{
+				return movie.title === titleToDelete
+			})
+
+			favoriteMovieList.splice(deleteMovieIndex,1)
+
+			res.json({
+				success: true,
+				
+			})
+
+		})
+//------------------------------------------------------------
